5f82556b223f0b43cadebcc37dd89d05328bc2df
Artem Savchuk <artemsavchuk01@gmail.com>
2017-02-14 17:18:04 +0300

    Added method 'addUserAnswer' to QuestionStringSchema

--- server/models/QuestionString.js
+++ server/models/QuestionString.js
@@ -6,7 +6,9 @@ let questionStringSchema = new Schema({
     questionText: String,
     usersAns: [String]
 });
-
+questionStringSchema.methods.addUserAnswer = function (ans) {
+    this.usersAns.push(ans);
+}
 //module.exports = questionStringSchema;
 //module.exports = mongoose.model('questionString',questionStringSchema);
 module.exports = function(db) {
