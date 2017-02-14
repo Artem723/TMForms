1976bfbd22429d4e6190256d14267916d9df457a
Artem Savchuk <artemsavchuk01@gmail.com>
2017-02-14 12:25:18 +0300

    Added proxy to the client

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ let express = require("express");
 let apiRoutes = require("./routes/apiRoutes");
 let morgan = require("morgan");
 let app = express()
+let port = 4000;
 
 app.use(morgan('dev'));
 app.use("/api",apiRoutes);
 
-app.listen(3000, function () {
-  console.log("Server has been started");
+app.listen(port, function () {
+  console.log("Server has been started on port" + port);
 })
 
 
