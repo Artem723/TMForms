8eada813fcd670a49c05000e4d03b76650c57c8e
Artem Savchuk <artemsavchuk01@gmail.com>
2017-03-20 17:02:47 +0300

    Added Styles to LogIn

--- src/Components/LogIn/index.js
+++ src/Components/LogIn/index.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react"
+import {
+    Col,
+    Alert,
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    Button
+} from "react-bootstrap"
+import { Link } from "react-router"
 import "./LogIn.css"
 
 export default class LogIn extends Component {
@@ -6,11 +15,16 @@ export default class LogIn extends Component {
         super(props);
         this.state = {
             login: "",
-            password: ""
+            password: "",
+            errorAlertText: null,//if the string then show alert
+            isLoginIncorrect: false,
+            isPasswordIncorrect: false
         }
         this.onChangeLogin = this.onChangeLogin.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onSignInHandler = this.onSignInHandler.bind(this);
+        this.onHideAlert = this.onHideAlert.bind(this);
+        this.getValidationState = this.getValidationState.bind(this);
     }
     componentDidMount() {
         if (this.props.token)
@@ -18,23 +32,31 @@ export default class LogIn extends Component {
     }
     onChangeLogin(e) {
         this.setState({
-            login: e.target.value
+            login: e.target.value,
+            isLoginIncorrect: false
         })
     }
     onChangePassword(e) {
         this.setState({
-            password: e.target.value
+            password: e.target.value,
+            isPasswordIncorrect: false
         })
     }
     onSignInHandler(e) {
         e.preventDefault();
         const { login, password } = this.state;
         if (!(/^[a-zA-Z0-9_]{3,15}$/.test(login))) {
-            alert("Login is incorrect.");
+            this.setState({
+                errorAlertText: "Login is incorrect",
+                isLoginIncorrect: true
+            });
             return;
         }
         if (password.length < 5) {
-            alert("Password is incorrect");
+            this.setState({
+                errorAlertText: "Password is Incorrect",
+                isPasswordIncorrect: true
+            });
             return;
         }
         const headers = {
@@ -55,12 +77,23 @@ export default class LogIn extends Component {
         fetch("/api/authentication", option)
             .then((response) => {
                 status = response.status;
+                if(status >= 500) 
+                    this.setState({
+                        errorAlertText: "Oh, we've got network issue. We try to do everything so that it does not happen again"
+                    });
                 return response.json();
             })
             .then((body) => {
-                if (status !== 200) {
-                    const message = body && body.message;
-                    alert("Something goes wrong. Status " + status + ". Message: " + message);
+                if (status === 404) {
+                    this.setState({
+                        errorAlertText: "Login is incorrect",
+                        isLoginIncorrect: true
+                    });
+                } else if(status === 401) {
+                    this.setState({
+                        errorAlertText: "Password is incorrect",
+                        isPasswordIncorrect: true
+                    });    
                 } else {
                     let token = body && body.token;
                     this.props.onLogInHandler(token);
@@ -71,17 +104,51 @@ export default class LogIn extends Component {
                 throw err;
             })
     }
+    onHideAlert() {
+        this.setState({
+            errorAlertText: null
+        })
+    }
+    getValidationState(type) {
+        const {isLoginIncorrect, isPasswordIncorrect} = this.state;
+        if(type==="login"){
+            return isLoginIncorrect ? "error" : null;
+        } else if(type==="password") {
+             return isPasswordIncorrect ? "error" : null;
+        }
+        return null;
+    }
     render() {
+        const { errorAlertText } = this.state;
+        let errorAlert = null;
+        if (errorAlertText) {
+            errorAlert = (
+                <Alert bsStyle="danger" onDismiss={this.onHideAlert}>
+                    {errorAlertText}
+                </Alert>
+            )
+        } 
         return (
-            <div className="LogIn-container">
-                <form onSubmit={this.onSignInHandler}>
-                    <div>Login</div>
-                    <input type="text" onChange={this.onChangeLogin} />
-                    <div>Password</div>
-                    <input type="password" onChange={this.onChangePassword} />
-                    <input type="submit" className="LogIn-signin-btn" value="Sign in" />
-                </form>
-            </div>
+            <section className="LogIn-container">
+                <Col sm={8} smOffset={2} lg={4} lgOffset={4}>
+                    <header>React Forms</header>
+                    <form onSubmit={this.onSignInHandler} className="bordered">
+                        <FormGroup validationState={this.getValidationState("login")}>
+                            <ControlLabel>Login</ControlLabel>
+                            <FormControl type="text" onChange={this.onChangeLogin} />
+                        </FormGroup>
+                        <FormGroup validationState={this.getValidationState("password")}>
+                            <ControlLabel>Password</ControlLabel>
+                            <FormControl type="password" onChange={this.onChangePassword} />
+                        </FormGroup>
+                        <Button type="submit" bsStyle="success" block>Sign in</Button>
+                    </form>
+                    {errorAlert}
+                    <section className="bordered">
+                        New to React Forms? <Link to="/SignUp">Create an account</Link>
+                    </section>
+                </Col>
+            </section>
         )
     }
 }
