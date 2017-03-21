09cc9bac5b242008bf879c4cbbd6fbfed69cec07
Artem Savchuk <artemsavchuk01@gmail.com>
2017-03-21 16:54:33 +0300

    Added glyphicon to button exit

--- src/Components/Header/HeaderLoggedIn/index.js
+++ src/Components/Header/HeaderLoggedIn/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { Link } from "react-router"
+import {Glyphicon} from "react-bootstrap"
 import "../header.css"
 
 export default class HeaderLoggedIn extends Component {
@@ -12,7 +13,7 @@ export default class HeaderLoggedIn extends Component {
             <ul className="navigation-bar">
                 <li><Link to="/">React Forms</Link></li>
                 <li><Link to="/">Home</Link></li>               
-                <li className="right"><a href="#" onClick={onLogOutHandler}>Exit</a></li>
+                <li className="right"><a href="#" onClick={onLogOutHandler}>Exit  <Glyphicon glyph="log-out" /></a></li>
                 <li className="right"><span className="user-login">user: {login}</span></li>
             </ul>
         )
