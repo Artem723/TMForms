ee6cb0d941c580b2a2c0c70ad5565e5554c0a08a
Artem Savchuk <artemsavchuk01@gmail.com>
2017-03-28 16:19:43 +0300

    Added the ability to srve static files from build

--- server/server.js
+++ server/server.js
@@ -3,11 +3,20 @@
 let express = require("express");
 let apiRoutes = require("./routes/apiRoutes");
 let morgan = require("morgan");
+let path= require("path");
 let app = express()
 let port = 4000;
 
+
+app.use(express.static(path.join(__dirname, "..",'build')));
+
 app.use(morgan('dev'));
 app.use("/api",apiRoutes);
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, "..","build", "index.html"));
+})
+
 
 app.listen(port, function () {
   console.log("Server has been started on port " + port);
